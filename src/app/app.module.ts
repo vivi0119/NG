285565112739558a41245e564f6cb5f1e4c8e993
wuwei <732595363@qq.com>
2019-11-27 16:12:25 +0800

    ng generate(g) cpmponent 组件名

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { MyC01Component } from './myc01';
 import { MyC02Component } from './myc02.component';
 import { MyC03Component } from './myc03/myc03.component';
+import { Myc04Component } from './myc04/myc04.component';
+import { Myc05Component } from './myc05/myc05.component';
+import { Myc06Component } from './myc06/myc06.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyC01Component,
     MyC02Component,
-    MyC03Component
+    MyC03Component,
+    Myc04Component,
+    Myc05Component,
+    Myc06Component
   ],
   imports: [
     BrowserModule,
